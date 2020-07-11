53f3f14afa6ea63df4fb8c6ed53c75496ae0967e
Brayan <brayansneider22@gmail.com>
2020-07-11 17:28:12 -0500

    update form

--- Taller5/js/code.js
+++ Taller5/js/code.js
@@ -9,3 +9,28 @@ function max_25(element) {
 
   }
 }
+
+function pass(element) {
+
+  let text = element.value;
+  if (text.length < 15 || text.length > 25 ) {
+      element.setCustomValidity('La longitud debe ser de minimo 15 caracteres y maximo 20 caracteres')
+
+  }
+}
+
+function confirm(element) {
+
+  let pass = document.getElementById('password')
+  if (element.value == pass.value){
+      element.pattern =  "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{12,25}$";
+      element.setCustomValidity('');
+  } else {
+      element.pattern =  '';
+      element.setCustomValidity('La contraseñas deben coincidir');
+  }
+  console.log(element.pattern );
+  console.log(element.checkValidity());
+
+
+}
