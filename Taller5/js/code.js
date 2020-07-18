ed8bf38dda92fca0360ecca690312cfb22a11429
Santiago Parra <santiago2180069@saber.uis.edu.co>
2020-07-17 20:53:09 -0500

    programar es mi pasion

--- Taller5/js/code.js
+++ Taller5/js/code.js
@@ -42,3 +42,33 @@ function pass(element) {
 
   }
 }
+function substringdireccion(element) {
+
+  let text = element.value;
+  
+  
+  if (text.length == 0 ) {
+      element.setCustomValidity('Rellene este campo');
+
+  } 
+  if( text.indexOf("cll") > -1 == false){
+    element.setCustomValidity('El campo debe comenzar con cll, av, avn, cra, trans');
+    
+  }
+  if( text.indexOf("cra") > -1 == false){
+    element.setCustomValidity('El campo debe comenzar con cll, av, avn, cra, trans');
+    
+  }
+  if( text.indexOf("av") > -1 == false){
+    element.setCustomValidity('El campo debe comenzar con cll, av, avn, cra, trans');
+    
+  }
+  if( text.indexOf("avn") > -1 == false){
+    element.setCustomValidity('El campo debe comenzar con cll, av, avn, cra, trans');
+    
+  }
+
+  
+ 
+}
+
